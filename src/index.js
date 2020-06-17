c35c92bc7a5242bbd3b14d4c625a8a67152ba04e
Alan Matkorski <alanmatkorski@gmail.com>
2020-06-16 21:54:08 -0300

    added bugsnag

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react';
 import App from './App';
 import './styles.scss';
 import 'bulma/bulma.sass';
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+
+Bugsnag.start({
+  apiKey: process.env.REACT_APP_BUGSNAG_KEY,
+  plugins: [new BugsnagPluginReact()]
+});
+
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );

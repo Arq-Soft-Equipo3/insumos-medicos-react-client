df58196f96f7acbbcbeaebaba4ce5b7173672338
Alan Matkorski <alanmatkorski@gmail.com>
2020-07-04 13:10:23 -0300

    made changes to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,37 +2,37 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { isAuthenticated } from '../services/auth';
 
-const Home = () => {
-  const welcomeMessage = isAuthenticated() ? 'Carga tu solicitud de insumos que nosotros nos encargaremos de derivarla a la organización correspondiente.' : 'Registrate en nuestro sistema para poder cargar tu solicitud de insumos que nosotros nos encargaremos de derivarla a la organización correspondiente.';
-
-  return (
-    <>
-      <Navbar />
-      <section className="hero">
-        <div className="hero-body">
-          <div className="container">
-            <section className="section">
-              <div className="columns">
-                <div className="column is-8 is-offset-2">
-                  <div className="content is-medium">
-                    <h1 className="title">Bienvenido!</h1>
-                    <p>{ welcomeMessage }</p>
-                  </div>
-                </div>
-              </div>
-            </section>
+const Home = () => (
+  <>
+    <Navbar />
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <section className="section">
             <div className="columns">
               <div className="column is-8 is-offset-2">
-                <figure className="image is-16by9">
-                  <img src="https://www.camarabaq.org.co/wp-content/uploads/2020/04/3461699-min-1200x650.jpg" alt="" />
-                </figure>
+                <div className="content is-medium">
+                  <h1 className="title">Bienvenido!</h1>
+                  <p>
+                    { isAuthenticated()
+                      ? 'Carga tu solicitud de insumos que nosotros nos encargaremos de derivarla a la organización correspondiente.'
+                      : 'Registrate en nuestro sistema para poder cargar tu solicitud de insumos que nosotros nos encargaremos de derivarla a la organización correspondiente.'}
+                  </p>
+                </div>
               </div>
+            </div>
+          </section>
+          <div className="columns">
+            <div className="column is-8 is-offset-2">
+              <figure className="image is-16by9">
+                <img src="https://www.camarabaq.org.co/wp-content/uploads/2020/04/3461699-min-1200x650.jpg" alt="" />
+              </figure>
             </div>
           </div>
         </div>
-      </section>
-    </>
-  );
-};
+      </div>
+    </section>
+  </>
+);
 
 export default Home;

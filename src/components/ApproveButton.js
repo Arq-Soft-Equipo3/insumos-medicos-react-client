9cf5f9d7501c55de32744f0e030698884cc5c24c
Alan Matkorski <alanmatkorski@gmail.com>
2020-07-19 21:26:23 -0300

    color enhancements

--- src/components/ApproveButton.js
+++ src/components/ApproveButton.js
@@ -4,7 +4,7 @@ import { Button, Icon } from 'react-bulma-components';
 
 const ApproveButton = ({ handleClick }) => (
   <Button
-    style={{ borderRadius: '50%', margin: '0 5px' }}
+    style={{ borderRadius: '50%', margin: '0 5px', backgroundColor: 'rgb(70 165 93)' }}
     className="is-icon-button"
     color="success"
     size="small"
--- src/components/RejectButton.js
+++ src/components/RejectButton.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const RejectButton = ({ handleClick }) => (
   <Button
-    style={{ borderRadius: '50%', margin: '0 5px' }}
+    style={{ borderRadius: '50%', margin: '0 5px', backgroundColor: 'rgb(228 72 94)' }}
     className="is-icon-button"
     color="danger"
     size="small"
